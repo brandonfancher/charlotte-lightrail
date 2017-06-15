6d932119879ad58c82d01dfde73aaefd11c1933f
Michael Wagner <mdwranger@gmail.com>
2017-06-15 13:01:45 -0400

    test wip

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -1,12 +1,33 @@
-import 'react-native';
+// import 'react-native';
+import { NativeModules } from 'react-native';
 import React from 'react';
+jest.mock('react-native-quick-actions', () => {
+  return {
+    popInitialAction: jest.fn(() => Promise.resolve(null)),
+    setShortcutItems: jest.fn(() => Promise.resolve()),
+    clearShortcutItems: jest.fn(() => Promise.resolve()),
+    isSupported: jest.fn(() => Promise.resolve())
+  }
+});
+import QuickAcitons from 'react-native-quick-actions';
+jest.mock('react-native-mapbox-gl');
 import Index from '../index.ios.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  );
+describe('lightrail', () => {
+  beforeEach(() => {
+    // jest.mock('QuickActions', () => {
+    //   return {
+    //     popInitialAction: jest.fn()
+    //   }
+    // });
+    // NativeModules.RNQuickActionManager = { popInitialAction: jest.fn(), initialAction: jest.fn() }
+  });
+  it('renders correctly', () => {
+    const tree = renderer.create(
+      <Index />
+    );
+  });
 });
