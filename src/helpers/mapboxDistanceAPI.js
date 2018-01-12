e8c91da5652b7cb40b5655c2e50d38c6dca4368d
Brandon Fancher <brandonfancher@gmail.com>
2018-01-12 09:54:11 -0500

    Remove deprecated Mapbox Distance API in favor of Matrix API.

--- src/helpers/mapboxDistanceAPI.js
+++ src/helpers/mapboxDistanceAPI.js
@@ -5,24 +5,10 @@ export const mapboxDistanceAPI = {
     const coordinates = destinations;
     coordinates.unshift(origin);
 
-    if (__DEV__) {
-      const endpoint = `directions-matrix/v1/mapbox/${mode}`;
-      const coordinatesString = coordinates.map(coordinate => coordinate.toString());
-      const coordinatesQuery = coordinatesString.join(';');
-      const url = `https://api.mapbox.com/${endpoint}/${coordinatesQuery}?access_token=${mapboxApiKey}`;
-      return fetch(url).then(res => res.json());
-    }
-
-    const endpoint = `distances/v1/mapbox/${mode}`;
-    const url = `https://api.mapbox.com/${endpoint}?access_token=${mapboxApiKey}`;
-    return fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        coordinates
-      })
-    }).then(res => res.json());
+    const endpoint = `directions-matrix/v1/mapbox/${mode}`;
+    const coordinatesString = coordinates.map(coordinate => coordinate.toString());
+    const coordinatesQuery = coordinatesString.join(';');
+    const url = `https://api.mapbox.com/${endpoint}/${coordinatesQuery}?access_token=${mapboxApiKey}`;
+    return fetch(url).then(res => res.json());
   }
 };
